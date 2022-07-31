0fe7d3abd18f7f9b39221f63f20703bc4f721e24
iranmilad <kazemi.milad@gmail.com>
2022-07-31 18:38:04 +0430

    a

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -25,12 +25,21 @@ class HomeController extends Controller
 
         $sumAll = (clone $query)->sum('amount');
 
-        $sumInMonth = (clone $query)->where([[DB::raw('pyear(created_at)'), $currentYear],
-            [DB::raw('pmonth(created_at)'), $currentMonth]])->sum('amount');
+        // $sumInMonth = (clone $query)->where([[DB::raw('pyear(created_at)'), $currentYear],
+        //     [DB::raw('pmonth(created_at)'), $currentMonth]])->sum('amount');
 
-        $chartData = (clone $query)->whereDate(DB::raw('pdate(created_at)'), '>=', $now->subMonths(6)->format('%Y-%m-%d'))->groupBy(DB::raw("pyear(created_at), pmonth(created_at)"))
-            ->orderByDesc(DB::raw("pyear(created_at), pmonth(created_at)"))
-            ->get([DB::raw("sum(amount) as sum,pyear(created_at) as year,pmonth(created_at) as month")]);
+        // $chartData = (clone $query)->whereDate(DB::raw('pdate(created_at)'), '>=', $now->subMonths(6)->format('%Y-%m-%d'))->groupBy(DB::raw("pyear(created_at), pmonth(created_at)"))
+        //     ->orderByDesc(DB::raw("pyear(created_at), pmonth(created_at)"))
+        //     ->get([DB::raw("sum(amount) as sum,pyear(created_at) as year,pmonth(created_at) as month")]);
+        $sumInMonth = (clone $query)->where([[DB::raw('created_at'), $currentYear],
+            [DB::raw('created_at'), $currentMonth]])->sum('amount');
+
+        $chartData = (clone $query)->whereDate(DB::raw('created_at'), '>=', $now->subMonths(6)->format('%Y-%m-%d'))->groupBy(DB::raw("created_at, created_at"))
+            ->orderByDesc(DB::raw("created_at, created_at"))
+            ->get([DB::raw("sum(amount) as sum,created_at as year,created_at as month")]);
+
+
+
         // dd($chartData->toArray());
         return view("admin.home", compact("sumAll", "sumInMonth", "chartData"));
     }
