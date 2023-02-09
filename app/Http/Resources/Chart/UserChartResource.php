ea283e83a2153e6bc95942caa2cbb32737978971
iranmilad <kazemi.milad@gmail.com>
2023-02-09 15:38:48 +0330

    a

--- app/Http/Resources/Chart/UserChartResource.php
+++ app/Http/Resources/Chart/UserChartResource.php
@@ -16,14 +16,29 @@ class UserChartResource extends JsonResource
     public function toArray($request)
     {
         $accessibility=$this->accessibility;
-        return [
-            "id" => $this->when($this->id !== null, $this->id),
-            "title" => $this->when($this->title !== null, $this->title),
-            "key" => $this->when($this->key !== null, $this->key),
-            "refresh_time" => $accessibility->refresh_time,
-            "feeder_url" => $this->when($this->feeder_url !== null, $this->feeder_url),
-            "active" => $accessibility->active,
-            "created_at" => $this->when($this->created_at !== null, jdate($this->created_at)->format(jDateFormat::S)),
-        ];
+        if (request()->secure())
+        {
+            return [
+                "id" => $this->when($this->id !== null, $this->id),
+                "title" => $this->when($this->title !== null, $this->title),
+                "key" => $this->when($this->key !== null, $this->key),
+                "refresh_time" => $accessibility->refresh_time,
+                "feeder_url" => $this->when($this->feeder_url !== null, "https://".$this->feeder_url),
+                "active" => $accessibility->active,
+                "created_at" => $this->when($this->created_at !== null, jdate($this->created_at)->format(jDateFormat::S)),
+            ];
+
+        }
+        else{
+            return [
+                "id" => $this->when($this->id !== null, $this->id),
+                "title" => $this->when($this->title !== null, $this->title),
+                "key" => $this->when($this->key !== null, $this->key),
+                "refresh_time" => $accessibility->refresh_time,
+                "feeder_url" => $this->when($this->feeder_url !== null, "http://".$this->feeder_url),
+                "active" => $accessibility->active,
+                "created_at" => $this->when($this->created_at !== null, jdate($this->created_at)->format(jDateFormat::S)),
+            ];
+        }
     }
 }
