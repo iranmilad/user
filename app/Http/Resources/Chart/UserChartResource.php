ea3a51390afe577a86f67db3085795ad18adc7d2
iranmilad <kazemi.milad@gmail.com>
2023-03-08 17:13:56 -0500

    a

--- app/Http/Resources/Chart/UserChartResource.php
+++ app/Http/Resources/Chart/UserChartResource.php
@@ -23,7 +23,7 @@ class UserChartResource extends JsonResource
                 "title" => $this->when($this->title !== null, $this->title),
                 "key" => $this->when($this->key !== null, $this->key),
                 "refresh_time" => $accessibility->refresh_time,
-                "feeder_url" => $this->when($this->feeder_url !== null, "http://".$this->feeder_url),
+                "feeder_url" => $this->when($this->feeder_url !== null, "http://".substr($this->feeder_url,16)),
                 "active" => $accessibility->active,
                 "created_at" => $this->when($this->created_at !== null, jdate($this->created_at)->format(jDateFormat::S)),
             ];
